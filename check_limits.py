3dd79a71164231c04470d15918367c9f8ad89153
Ravi Kudumu <ravikudumu@gmail.com>
2021-03-08 20:48:48 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,32 @@
+batter_parameter = {"temperature": [0, 45, "Temperature", 5],
+                    "soc": [20, 80, "Soc", 5],
+                    "charge_rate": [0, 0.8, "Charge Rate", 5]}
 
-def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
 
-  return True
+def battery_status(battery_parameter_name):
+    parameter_range = batter_parameter[battery_parameter_name["battery_parameter"]]
+    low = parameter_range[0]
+    high = parameter_range[1]
+    low_warning = parameter_range[0] * (100 - parameter_range[3])
+    high_warning = parameter_range[1] * (100 - parameter_range[3])
+    value = battery_parameter_name["value"]
+    if value <= low or value >= high:
+        print('{} is out of range!'.format(parameter_range[2]))
+        return False
+    elif value <= low_warning:
+        print('{} nearing low threshold value!'.format(parameter_range[2]))
+    elif value >= high_warning:
+        print('{} nearing high threshold value!'.format(parameter_range[2]))
+        return True
+    return True
 
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+    assert (battery_status({"battery_parameter": "temperature", "value": 25}) is True)
+    assert (battery_status({"battery_parameter": "soc", "value": 70}) is True)
+    assert (battery_status({"battery_parameter": "soc", "value": 21}) is True)
+    assert (battery_status({"battery_parameter": "charge_rate", "value": 0.7}) is True)
+    assert (battery_status({"battery_parameter": "temperature", "value": 50}) is False)
+    assert (battery_status({"battery_parameter": "soc", "value": 85}) is False)
+    assert (battery_status({"battery_parameter": "soc", "value": 19}) is False)
+    assert (battery_status({"battery_parameter": "charge_rate", "value": 0}) is False)
