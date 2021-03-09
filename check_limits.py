aec5da63eb640175e9ccc50d8ccf175938ad0284
Ravi Kudumu <ravikudumu@gmail.com>
2021-03-09 19:51:55 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,32 +1,36 @@
-batter_parameter = {"temperature": [0, 45, "Temperature", 5],
-                    "soc": [20, 80, "Soc", 5],
-                    "charge_rate": [0, 0.8, "Charge Rate", 5]}
+battery_parameter = {"temperature": [0, 45, "Temperature", 5, 'Temperatur'],
+                    "soc": [20, 80, "Soc", 5, "SOC"],
+                    "charge_rate": [0, 0.8, "Charge Rate", 5, "Ladestrom"]}
+language_text = {"failed": ["{} is out of range!", "{} ist außerhalb der Reichweite!"],
+                 "warning": ["Warning: {} is nearing the {} limit",
+                             "Warnung:Der Wert {} nähert sich der Grenze {}"]}
 
-
-def battery_status(battery_parameter_name):
-    parameter_range = batter_parameter[battery_parameter_name["battery_parameter"]]
+language_code = {"EN": 0, "DE": 1}
+def battery_status(battery_parameter_name, language):
+    parameter_range = battery_parameter[battery_parameter_name["battery_parameter"]]
     low = parameter_range[0]
     high = parameter_range[1]
-    low_warning = parameter_range[0] * (100 - parameter_range[3])
-    high_warning = parameter_range[1] * (100 - parameter_range[3])
+    low_warning = parameter_range[0] + ( parameter_range[1] * parameter_range[3])/100
+    high_warning = parameter_range[1] * (100 - parameter_range[3])/100
     value = battery_parameter_name["value"]
     if value <= low or value >= high:
-        print('{} is out of range!'.format(parameter_range[2]))
+        print(language_text["failed"][language_code[language]].format(parameter_range[2]))
         return False
-    elif value <= low_warning:
-        print('{} nearing low threshold value!'.format(parameter_range[2]))
-    elif value >= high_warning:
-        print('{} nearing high threshold value!'.format(parameter_range[2]))
-        return True
+    compare_battery_param_value(low_warning, value, "lower", parameter_range, language)
+    compare_battery_param_value(value, high_warning, "higher",parameter_range,language)
     return True
+
+def compare_battery_param_value(lower_value, upper_value, boundary,parameter_range, language):
+    if lower_value >= upper_value:
+        print(language_text["warning"][language_code[language]].format(parameter_range[2], boundary) )
+
+
 
 
 if __name__ == '__main__':
-    assert (battery_status({"battery_parameter": "temperature", "value": 25}) is True)
-    assert (battery_status({"battery_parameter": "soc", "value": 70}) is True)
-    assert (battery_status({"battery_parameter": "soc", "value": 21}) is True)
-    assert (battery_status({"battery_parameter": "charge_rate", "value": 0.7}) is True)
-    assert (battery_status({"battery_parameter": "temperature", "value": 50}) is False)
-    assert (battery_status({"battery_parameter": "soc", "value": 85}) is False)
-    assert (battery_status({"battery_parameter": "soc", "value": 19}) is False)
-    assert (battery_status({"battery_parameter": "charge_rate", "value": 0}) is False)
+    assert (battery_status({"battery_parameter": "temperature", "value": 25}, "EN") )is True
+    assert (battery_status({"battery_parameter": "soc", "value": 77},"DE") is True)
+    assert (battery_status({"battery_parameter": "soc", "value": 23},"EN") is True)
+    assert (battery_status({"battery_parameter": "temperature", "value": 50}, "EN") is False)
+    assert (battery_status({"battery_parameter": "temperature", "value": 50}, "DE") is False)
+    assert (battery_status({"battery_parameter": "charge_rate", "value": 0}, 'EN') is False)
